0cb12646f1d8ce9751292599840ffca4925c700f
aTemporal <aTemporal@sol.com>
2021-07-08 19:17:18 -0500

    Initialized emoticons array with some popular global twitch emotes.

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(helmet());
 
-const emoticons = [];
+const emoticons = ['LUL', 'PogChamp', 'HeyGuys', 'DansGame', '4Head', 'Kreygasm']; // Initializing with some popular global emotes
 let sneeze = false;
 
 // Define configuration options
