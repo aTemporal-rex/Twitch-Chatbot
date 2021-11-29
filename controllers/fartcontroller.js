1259a9bfd274541cb5d4459073afbd663788e712
aTemporal <>
2021-11-28 23:11:49 -0600

    Fixed typo

--- controllers/fartcontroller.js
+++ controllers/fartcontroller.js
@@ -6,7 +6,7 @@ function onFartHandler(target, client, context) {
     if (Math.random() <= CHANCE) {
         const fart = farts[Math.floor(Math.random() * farts.length)];
         const fartTarget = context['display-name'];
-        client.say(target, `*${fart}* jlastGuobafart Sorry ${fartTarget}, I get gassy thinking about all the presents I'm gonna get peepoHappy!`);
+        client.say(target, `*${fart}* jlastGuobafart Sorry ${fartTarget}, I get gassy thinking about all the presents I'm gonna get! peepoHappy`);
     }
 }
 
