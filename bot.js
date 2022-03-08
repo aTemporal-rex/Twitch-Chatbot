55cafb5d58be0cc01bff18e402754f2b91ec2bc7
aTemporal <>
2022-03-07 19:39:37 -0500

    Changed the greeting.

--- bot.js
+++ bot.js
@@ -98,7 +98,7 @@ async function onConnectedHandler (addr, port) {
     client.color('HotPink');
 
     // Declare the glorious arrival of Bunni Senpai Bot
-    client.say(process.env.CHANNEL_NAME, "Bunni Senpai has arrived!! peepoHey");
+    client.say(process.env.CHANNEL_NAME, "Bunni Senpai has arrived!! GuobaHi");
 }
 
 // This is necessary to prevent heroku from disconnecting
